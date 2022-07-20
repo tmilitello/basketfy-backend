10227880262a40d5230e73e4ea6c7180f7490eb7
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-07-20 01:18:42 -0500

    adds stuff

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "localhost:8080", "basketfy.io"
+    origins "https://basketfy-2022.herokuapp.com"
 
     resource "*",
       headers: :any,
