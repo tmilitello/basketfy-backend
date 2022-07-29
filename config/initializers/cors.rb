9ab2d4dd02e516341d48a26de76c5ecc2a2982ac
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-07-29 11:02:22 -0500

    changes cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "basketfy.io", "localhost:3000", "localhost:8080",  "fierce-coast-33664.herokuapp", "fierce-coast-33664.herokuapp.com", "fierce-coast-33664.herokuapp.com", "https://fierce-coast-33664.herokuapp.com"
+    origins "basketfy.io", "localhost:3000", "localhost:8080"
 
     resource "*",
       headers: :any,
