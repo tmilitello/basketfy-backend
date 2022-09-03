eeed080aeaf3acdcd1863657e65a7ba81a9e2d5c
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-09-03 21:31:53 +0900

    adds stuff

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "basketfy.io", "www.basketfy.io", "basketfy.io","localhost:3000", "localhost:8080",  "limitless-mesa-74394.herokuapp", "limitless-mesa-74394.herokuapp.com", "limitless-mesa-74394.herokuapp.com", "https://limitless-mesa-74394.herokuapp.com"
+    origins "basketfy.io", "www.basketfy.io", "basketfy.io","localhost:3000", "localhost:8080",  "https://63133dc4d407750884f93c8d--dreamy-swan-af1c13.netlify.app", "63133dc4d407750884f93c8d--dreamy-swan-af1c13.netlify.app", "https://63133dc4d407750884f93c8d--dreamy-swan-af1c13.netlify"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
